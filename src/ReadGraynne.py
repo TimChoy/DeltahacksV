e1c398d827f6995fd618646285f728ea029fa594
Lewis Able Rafuse <rafusel@mcmaster.ca>
2019-01-26 17:41:21 -0500

    Wrote flattening function.

--- src/ReadGraynne.py
+++ src/ReadGraynne.py
@@ -5,12 +5,21 @@
 
 from numpy import *
 from PIL import Image
+from copy import *
 
 ## Function readGraynne
 #  Parameter image_name: string of name of image file
-#  Description: Takes a string of the name of image and returns a 2D array of 
+#  Description: Takes a string of the name of image and returns a 2D array of
 #  grayscale values of the image. Grayscale pixel value ranges from 0 to 255
 def readGraynne(image_name):
     img = Image.open(image_name, 'r').convert('L')
     data = asarray(img)
     return data
+
+def flatten(list2D):
+    toFlatten = copy(list2D)
+    flattened = []
+    for i in range(length(toFlatten)):
+        for j in range(length(toFlatten[i])):
+            flattened.append(toFlatten[i][j])
+    return flattened
