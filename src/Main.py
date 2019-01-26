24cce2b104f27fc8f19d599d6649c6673b89d42a
Timothy Choy <choyt2@mcmaster.ca>
2019-01-26 17:25:59 -0500

    Modify Main to create gui

--- src/Main.py
+++ src/Main.py
@@ -2,16 +2,31 @@
 #  Entry for DeltaHacks V
 #  Tim Choy, Sam Crawford, Lewis Rafuse, and Cameron Dufault
 
-import sys
-from ReadGraynne import *
+#import sys
+import PySimpleGUI as sg
+#from ReadGraynne import *
 
 def main():
-    imgDir = sys.argv[1]
-    printList = readGraynne(imgDir)
+    layout = [      
+            [sg.Text('Please enter your Name, Address, Phone')],      
+            [sg.Text('Name', size=(15, 1)), sg.InputText('name')],      
+            [sg.Text('Address', size=(15, 1)), sg.InputText('address')],      
+            [sg.Text('Phone', size=(15, 1)), sg.InputText('phone')],      
+            [sg.Submit(), sg.Cancel()]      
+            ]
+    
+    window = sg.Window('Graynne GUI').Layout(layout)
+    button, values = window.Read()
 
-    for i in printList:
-        for j in i:
-            print(str(j), end=" ")
-        print("\n")
+    print(button, values[0])
+
+
+    # imgDir = sys.argv[1]
+    # printList = readGraynne(imgDir)
+
+    # for i in printList:
+    #     for j in i:
+    #         print(str(j), end=" ")
+    #     print("\n")
 
 main()
