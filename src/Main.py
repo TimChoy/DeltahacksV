a49b8a759571c84dc2c1b0d3030de63019cdbde9
Timothy Choy <choyt2@mcmaster.ca>
2019-01-26 16:44:14 -0500

    Improve main printing

--- src/Main.py
+++ src/Main.py
@@ -2,7 +2,16 @@
 #  Entry for DeltaHacks V
 #  Tim Choy, Sam Crawford, Lewis Rafuse, and Cameron Dufault
 
+import sys
+from ReadGraynne import *
+
 def main():
-    print("To implement")
+    imgDir = sys.argv[1]
+    printList = readGraynne(imgDir)
+
+    for i in printList:
+        for j in i:
+            print(str(j), end=" ")
+        print("\n")
 
 main()
