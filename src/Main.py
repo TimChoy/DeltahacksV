e8acaeb415394a11289c077402165771747abeea
Sam Crawford <sam.crawford82@gmail.com>
2019-01-26 18:07:44 -0500

    Added output GUI

--- src/Main.py
+++ src/Main.py
@@ -7,15 +7,15 @@ import PySimpleGUI as sg
 from ReadGraynne import *
 
 def main():
-    layout = [      
-             [sg.Text("Choose an Input File", size=(35, 1))], 
-             [sg.Text("Input File", size=(15,1), auto_size_text=False, justification="right"),
-              sg.InputText("Select File"), sg.FileBrowse()],     
-             [sg.Submit(), sg.Cancel()]    
-             ]
+    fileDialogue = [      
+        [sg.Text("Choose an Input File", size=(35, 1))], 
+        [sg.Text("Input File", size=(15,1), auto_size_text=False, justification="right"),
+         sg.InputText("Select File"), sg.FileBrowse()],     
+        [sg.CloseButton("Submit"), sg.CloseButton("Cancel")]    
+    ]
     
-    window = sg.Window('Graynne GUI').Layout(layout)
-    button, values = window.Read()
+    fileWindow = sg.Window('Graynne GUI').Layout(fileDialogue)
+    button, values = fileWindow.Read()
 
     print(button, values[0])
     
@@ -25,5 +25,18 @@ def main():
         for j in i:
             print(str(j), end=" ")
         print("\n")
+
+    #garbage = input("Press 'Enter' to continue...")
+
+    darkPhase = 0.2614223074892 # For testing
+
+    outString = "The percentage of dark phase in the specified image is {:.2%}".format(darkPhase)
+    outDialogue = [
+        [sg.Text(outString)],
+        [sg.CloseButton("OK")]
+    ]
+
+    outWindow = sg.Window('Graynne GUI').Layout(outDialogue)
+    button = outWindow.Read()
 
 main()
