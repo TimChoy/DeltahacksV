4dfb09562c8ba00cdc1b4460f4af41cb199080be
Sam Crawford <sam.crawford82@gmail.com>
2019-01-26 22:52:01 -0500

    Implemented left and right diff functions

--- src/diffs.py
+++ src/diffs.py
@@ -14,16 +14,30 @@ def fiveDown(readRGB):
 
 def fiveRight(readRGB):
     diff = empty(len(readRGB))
+    for i in range(len(readRGB)):
+        if (i % 1024 < 1019):
+            diff[i] = readRGB[i] - readRGB[i + 5]
+        else:
+            diff[i] = 0
 
     return diff
 
 def fiveLeft(readRGB):
     diff = empty(len(readRGB))
+    for i in range(len(readRGB)):
+        if (i % 1024 > 4):
+            diff[i] = readRGB[i] - readRGB[i - 5]
+        else:
+            diff[i] = 0
 
     return diff
 
 def tenUp(readRGB):
     diff = empty(len(readRGB))
+    for i in range(1024*10):
+        diff[i] = 0
+    for i in range(1024*10, len(readRGB)):
+        diff[i] =  readRGB[i] - readRGB[i - 10*1024]
 
     return diff
 
@@ -34,10 +48,20 @@ def tenDown(readRGB):
 
 def tenRight(readRGB):
     diff = empty(len(readRGB))
+    for i in range(len(readRGB)):
+        if (i % 1024 < 1014):
+            diff[i] = readRGB[i] - readRGB[i + 10]
+        else:
+            diff[i] = 0
 
     return diff
 
 def tenLeft(readRGB):
     diff = empty(len(readRGB))
+    for i in range(len(readRGB)):
+        if (i % 1024 > 9):
+            diff[i] = readRGB[i] - readRGB[i - 10]
+        else:
+            diff[i] = 0
 
     return diff
