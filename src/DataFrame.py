4f538e47850160327951de157251e94ef3616618
Sam Crawford <sam.crawford82@gmail.com>
2019-01-27 03:16:32 -0500

    Added DataFrame module

---
+++ src/DataFrame.py
@@ -0,0 +1,75 @@
+# DataFrame.py
+# Work for handling DataFrames
+
+from pandas import DataFrame
+
+from diffs import *
+
+def genDataFrame(readRGB, solvedCat):
+    pixels_dict = {}
+    pixelsFrame = DataFrame(pixels_dict)
+
+    pixelsFrame['originalRGB'] = readRGB
+    pixelsFrame['solved Cat']  = solvedCat
+
+    # Convention for order: up, left, right, down
+
+    pixelsFrame['diffFiveUp']    = fiveUp(readRGB)
+    pixelsFrame['diffTenUp']     = tenUp(readRGB)
+    pixelsFrame['diffFiveLeft']  = fiveLeft(readRGB)
+    pixelsFrame['diffTenLeft']   = tenLeft(readRGB)
+    pixelsFrame['diffFiveRight'] = fiveRight(readRGB)
+    pixelsFrame['diffTenRight']  = tenRight(readRGB)
+    pixelsFrame['diffFiveDown']  = fiveDown(readRGB)
+    pixelsFrame['diffTenDown']   = tenDown(readRGB)
+
+    pixelsFrame['diffFiveUpLeft']  = fiveUpLeft(readRGB)
+    pixelsFrame['diffTenUpLeft']   = tenUpLeft(readRGB)
+    pixelsFrame['diffFiveUpRight'] = fiveUpRight(readRGB)
+    pixelsFrame['diffTenUpRight']  = tenUpRight(readRGB)
+
+    pixelsFrame['diffFiveUp'].fillna(pixelsFrame['diffFiveUp'].mean(), inplace = True)
+    pixelsFrame['diffTenUp'].fillna(pixelsFrame['diffTenUp'].mean(), inplace = True)
+    pixelsFrame['diffFiveLeft'].fillna(pixelsFrame['diffFiveLeft'].mean(), inplace = True)
+    pixelsFrame['diffTenLeft'].fillna(pixelsFrame['diffTenLeft'].mean(), inplace = True)
+    pixelsFrame['diffFiveRight'].fillna(pixelsFrame['diffFiveRight'].mean(),inplace = True)
+    pixelsFrame['diffTenRight'].fillna(pixelsFrame['diffTenRight'].mean(), inplace = True)
+    pixelsFrame['diffFiveDown'].fillna(pixelsFrame['diffFiveDown'].mean(), inplace= True)
+    pixelsFrame['diffTenDown'].fillna(pixelsFrame['diffTenDown'].mean(), inplace = True)
+
+    pixelsFrame['diffFiveUpLeft'].fillna(pixelsFrame['diffFiveUpLeft'].mean(), inplace= True)
+    pixelsFrame['diffTenUpLeft'].fillna(pixelsFrame['diffTenUpLeft'].mean(), inplace= True)
+    pixelsFrame['diffFiveUpRight'].fillna(pixelsFrame['diffFiveUpRight'].mean(), inplace= True)
+    pixelsFrame['diffTenUpRight'].fillna(pixelsFrame['diffTenUpRight'].mean(), inplace= True)
+
+    pixelsFrame["solved Cat"] = pixelsFrame["solved Cat"].apply(category)
+
+    pixelsFrame["diffFiveUp"]    = pixelsFrame["diffFiveUp"].apply(categoryYo)
+    pixelsFrame["diffTenUp"]     = pixelsFrame["diffTenUp"].apply(categoryYo)
+    pixelsFrame["diffFiveLeft"]  = pixelsFrame["diffFiveLeft"].apply(categoryYo)
+    pixelsFrame["diffTenLeft"]   = pixelsFrame["diffTenLeft"].apply(categoryYo)
+    pixelsFrame["diffFiveRight"] = pixelsFrame["diffFiveRight"].apply(categoryYo)
+    pixelsFrame["diffTenRight"]  = pixelsFrame["diffTenRight"].apply(categoryYo)
+    pixelsFrame["diffFiveDown"]  = pixelsFrame["diffFiveDown"].apply(categoryYo)
+    pixelsFrame["diffTenDown"]   = pixelsFrame["diffTenDown"].apply(categoryYo)
+
+    pixelsFrame['diffFiveUpLeft']  = pixelsFrame['diffFiveUpLeft'].apply(categoryYo)
+    pixelsFrame['diffTenUpLeft']   = pixelsFrame['diffTenUpLeft'].apply(categoryYo)
+    pixelsFrame['diffFiveUpRight'] = pixelsFrame['diffFiveUpRight'].apply(categoryYo)
+    pixelsFrame['diffTenUpRight']  = pixelsFrame['diffTenUpRight'].apply(categoryYo)
+
+    return pixelsFrame
+
+def category(data):
+    if data > 30000:
+        return 1
+    else:
+        return 0
+    
+def categoryYo(data):
+    if (data > -3000 and data < 3000):
+        return 2
+    elif (data < -3000):
+        return 1
+    else:
+        return 0
--- src/grain.py
+++ src/grain.py
@@ -4,13 +4,13 @@ from numpy import *
 import pandas as pd
 from PIL import Image
 from copy import *
-from diffs import *
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
-pixels_dict = {}
+from DataFrame import genDataFrame
+
 readRGB     = []
 solvedCat   = []
 numOfPics   = 20
@@ -28,73 +28,18 @@ for i in range(numOfPics):
 readRGB = reshape(readRGB, int(1048576 * numOfPics))
 solvedCat = reshape(solvedCat,int(1048576 * numOfPics))
 
-pixelsFrame = pd.DataFrame(pixels_dict)
+dataFrame = genDataFrame(readRGB, solvedCat)
 
-pixelsFrame['originalRGB'] = readRGB
-pixelsFrame['solved Cat']  = solvedCat
+x = dataFrame.drop('solved Cat', axis = 1)
+y = dataFrame['solved Cat']
 
-# Convention for order: up, left, right, down
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
-pixelsFrame['diffFiveUp']    = fiveUp(readRGB)
-pixelsFrame['diffTenUp']     = tenUp(readRGB)
-pixelsFrame['diffFiveLeft']  = fiveLeft(readRGB)
-pixelsFrame['diffTenLeft']   = tenLeft(readRGB)
-pixelsFrame['diffFiveRight'] = fiveRight(readRGB)
-pixelsFrame['diffTenRight']  = tenRight(readRGB)
-pixelsFrame['diffFiveDown']  = fiveDown(readRGB)
-pixelsFrame['diffTenDown']   = tenDown(readRGB)
+logmodel = LogisticRegression()
+logmodel.fit(x_train, y_train)
+predictions = logmodel.predict(x_test)
 
-pixelsFrame['diffFiveUpLeft']  = fiveUpLeft(readRGB)
-pixelsFrame['diffTenUpLeft']   = tenUpLeft(readRGB)
-pixelsFrame['diffFiveUpRight'] = fiveUpRight(readRGB)
-pixelsFrame['diffTenUpRight']  = tenUpRight(readRGB)
-
-pixelsFrame['diffFiveUp'].fillna(pixelsFrame['diffFiveUp'].mean(), inplace = True)
-pixelsFrame['diffTenUp'].fillna(pixelsFrame['diffTenUp'].mean(), inplace = True)
-pixelsFrame['diffFiveLeft'].fillna(pixelsFrame['diffFiveLeft'].mean(), inplace = True)
-pixelsFrame['diffTenLeft'].fillna(pixelsFrame['diffTenLeft'].mean(), inplace = True)
-pixelsFrame['diffFiveRight'].fillna(pixelsFrame['diffFiveRight'].mean(),inplace = True)
-pixelsFrame['diffTenRight'].fillna(pixelsFrame['diffTenRight'].mean(), inplace = True)
-pixelsFrame['diffFiveDown'].fillna(pixelsFrame['diffFiveDown'].mean(), inplace= True)
-pixelsFrame['diffTenDown'].fillna(pixelsFrame['diffTenDown'].mean(), inplace = True)
-
-pixelsFrame['diffFiveUpLeft'].fillna(pixelsFrame['diffFiveUpLeft'].mean(), inplace= True)
-pixelsFrame['diffTenUpLeft'].fillna(pixelsFrame['diffTenUpLeft'].mean(), inplace= True)
-pixelsFrame['diffFiveUpRight'].fillna(pixelsFrame['diffFiveUpRight'].mean(), inplace= True)
-pixelsFrame['diffTenUpRight'].fillna(pixelsFrame['diffTenUpRight'].mean(), inplace= True)
-
-count = 0
-def category(data):
-    if data > 30000:
-        return 1
-    else:
-        count = 1
-        return 0
-
-def categoryYo(data):
-    if (data > -3000 and data < 3000):
-        return 2
-    elif (data < -3000):
-        return 1
-    else:
-        return 0
-
-pixelsFrame["solved Cat"] = pixelsFrame["solved Cat"].apply(category)
-
-pixelsFrame["diffFiveUp"]    = pixelsFrame["diffFiveUp"].apply(categoryYo)
-pixelsFrame["diffTenUp"]     = pixelsFrame["diffTenUp"].apply(categoryYo)
-pixelsFrame["diffFiveLeft"]  = pixelsFrame["diffFiveLeft"].apply(categoryYo)
-pixelsFrame["diffTenLeft"]   = pixelsFrame["diffTenLeft"].apply(categoryYo)
-pixelsFrame["diffFiveRight"] = pixelsFrame["diffFiveRight"].apply(categoryYo)
-pixelsFrame["diffTenRight"]  = pixelsFrame["diffTenRight"].apply(categoryYo)
-pixelsFrame["diffFiveDown"]  = pixelsFrame["diffFiveDown"].apply(categoryYo)
-pixelsFrame["diffTenDown"]   = pixelsFrame["diffTenDown"].apply(categoryYo)
-
-pixelsFrame['diffFiveUpLeft']  = pixelsFrame['diffFiveUpLeft'].apply(categoryYo)
-pixelsFrame['diffTenUpLeft']   = pixelsFrame['diffTenUpLeft'].apply(categoryYo)
-pixelsFrame['diffFiveUpRight'] = pixelsFrame['diffFiveUpRight'].apply(categoryYo)
-pixelsFrame['diffTenUpRight']  = pixelsFrame['diffTenUpRight'].apply(categoryYo)
-
+print(confusion_matrix(y_test, predictions))
 
 # pixelsFrame.to_csv("output.csv")
 
@@ -110,18 +55,3 @@ pixelsFrame['diffTenUpRight']  = pixelsFrame['diffTenUpRight'].apply(categoryYo)
 #         pixelsFrame = pd.concat([pixelsFrame,chunk])
 
 # print("We read it good :)")
-
-x = pixelsFrame.drop('solved Cat', axis = 1)
-y = pixelsFrame['solved Cat']
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-
-logmodel = LogisticRegression()
-logmodel.fit(x_train, y_train)
-predictions = logmodel.predict(x_test)
-
-print(type(x_test))
-print(predictions)
-print(x_test)
-
-print(confusion_matrix(y_test, predictions))
