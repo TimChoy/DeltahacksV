e48e2516d6d65635c0b5488323baff1276c115bd
Sam Crawford <sam.crawford82@gmail.com>
2019-01-27 06:38:06 -0500

    Added number retrieval from file path

--- src/Main.py
+++ src/Main.py
@@ -7,6 +7,28 @@ import PySimpleGUI as sg
 from ReadGraynne import *
 
 def main():
+
+    # # Assumes Target 1
+    # pics     = 1
+    # fileRRGB = "../../fake_microstructure/Target_1/image_"
+    # fileMask = "../../fake_microstructure/Target_1/p2mask_np_"
+    # fileName = "../../fake_microstructure/Target_1/image_" + str(num) + ".png"
+
+    # readRGB     = genReadRGB(fileRRGB, pics)
+    # mask        = genMask(fileMask, pics)
+    # dataFrame   = genDataFrame(readRGB, mask)
+
+    # x_train, x_test, y_train, y_test = genTrainTest(dataFrame)
+    # logmodel    = genLogModel(x_train, y_train)
+    # predictions = genPredictions(fileName, logmodel)
+    # conMatrix   = genConMatrix(logmodel, x_test, y_test)
+
+    # writeGraynne(predictions)
+
+    # print(conMatrix)
+    # print("The accuracy of this run is {:.2%}".format(calcAccuracy(conMatrix)))
+    # ####################################################################
+    # Assumes 
     fileDialogue = [      
         [sg.Text("Choose an Input File", size=(35, 1))], 
         [sg.Text("Input File", size=(15,1), auto_size_text=False, justification="right"),
@@ -17,9 +39,19 @@ def main():
     fileWindow = sg.Window('Graynne GUI').Layout(fileDialogue)
     button, values = fileWindow.Read()
 
-    print(button, values[0])
+    filePath = values[0]
+    print(button, filePath)
+
+    if filePath[-6] == "_":
+        num = int(filePath[-5])
+    elif filePath[-7] == "_":
+        num = int(filePath[-6:-4])
+    elif filePath[-8] == "_":
+        num = int(filePath[-7:-4]) 
+    else:
+        print("BAD") # TODO: raise exception
     
-    printList = readGraynne(values[0])
+    printList = readGraynne(filePath)
 
     #garbage = input("Press 'Enter' to continue...")
 
