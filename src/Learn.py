e9f26fb09e6c49a04224acea11d80c75172b4fd1
Sam Crawford <sam.crawford82@gmail.com>
2019-01-27 01:05:40 -0500

    Improved Learn.py

--- src/Learn.py
+++ src/Learn.py
@@ -7,15 +7,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
-X = pixelsFrame.drop('solved Cat', axis = 1)
-y = pixelsFrame['solved Cat']
+# chunksize = 10 ** 6
+# chunks = pd.read_table("output.csv", chunksize=chunksize)
+# pixelsFrame = pd.DataFrame()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
+# for chunk in pd.read_csv("output.csv", chunksize=chunksize):
+#     process(chunk)
 pixelsFrame = pd.read_csv("output.csv")
 
+print("We read it good :)")
+
+x = pixelsFrame.drop('solved Cat', axis = 1)
+y = pixelsFrame['solved Cat']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
 logmodel = LogisticRegression()
-logmodel.fit(X_train, y_train)
-predictions = logmodel.predict(X_test)
+logmodel.fit(x_train, y_train)
+predictions = logmodel.predict(x_test)
 
 print(confusion_matrix(y_test, predictions))
