1328939d2a8b92b437883a7d4406cc57d36a5e2c
Lewis Able Rafuse <rafusel@mcmaster.ca>
2019-01-27 02:12:17 -0500

    Changed writeGraynne

--- src/ReadGraynne.py
+++ src/ReadGraynne.py
@@ -21,12 +21,17 @@ def readGraynne(image_name):
 
 ## Function writeGraynne
 #  Parameter data: a numpy 1D array
-#  Description: Takes in a 1D numpy array and turns it into a 2D array, and 
+#  Description: Takes in a 1D numpy array and turns it into a 2D array, and
 #  outputs a .png file named output.png.
 def writeGraynne(data):
     data2D = np.reshape(data, (-1, sqrt(data.size)))
+    for i in range(len(data2D)):
+        if(data2D[i] == 1):
+            data2D[i] = 65536
+        else:
+            data2D[i] = 0
     img = Image.fromarray(data2D, "I")
     img.save("output.png")
-    
+
 def flatten(list2D):
     return [item for sublist in list2D for item in sublist]
